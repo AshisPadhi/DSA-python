a951c94425bf4bb055eb988f9d3af374cdd41e9a
AshisPadhi <ashispadhi08@gmail.com>
2023-07-31 02:40:43 +0530

    Added append function

--- LinkedList/LL1.py
+++ LinkedList/LL1.py
@@ -15,7 +15,19 @@ class LinkedList:
         while temp:
             print(temp.value)
             temp=temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length += 1
 
 myLinkedList=LinkedList(4)
+myLinkedList.append(5)
+myLinkedList.append(8)
 #print(myLinkedList.head.value)
 myLinkedList.printList()
