3fa3432eecf6214ae1642376df50396895b82f54
AshisPadhi <ashispadhi08@gmail.com>
2023-08-04 23:49:38 +0530

    added prepend method

--- LinkedList/LL1.py
+++ LinkedList/LL1.py
@@ -26,8 +26,46 @@ class LinkedList:
             self.tail=new_node
         self.length += 1
 
+    def prepend(self, val):
+        tmp=Node(val)
+        if not self.head:
+            self.head=tmp
+            self.head.next=None
+        else:
+            tmp.next=self.head
+            self.head=tmp
+
+    def pop (self):
+        tmp=self.head
+        prev=self.tail
+        val=None
+        if tmp==prev:
+            val=tmp.value
+            self.head.next=None
+            self.tail.next=None
+            self.length=0
+        elif tmp.value==None:
+            val=None
+        else:
+            while tmp.next:
+                prev=tmp
+                tmp=tmp.next
+            val=tmp.value
+            self.tail=prev
+            self.tail.next=None
+            self.length-=1
+        return val
+
+
 myLinkedList=LinkedList(4)
 myLinkedList.append(5)
 myLinkedList.append(8)
+myLinkedList.append(1)
 #print(myLinkedList.head.value)
 myLinkedList.printList()
+#print("popping now")
+#print(myLinkedList.pop())
+#print(myLinkedList.pop())
+myLinkedList.prepend(6)
+print("printing after prepending")
+print(myLinkedList.printList())
